e2b3a06d98da233fae565ee6acd4401731b524f6
Stanislav-Rybonka <stanislav1991r@gmail.com>
2016-08-23 22:10:46 +0300

    First commit. Tickets sustem version 1.1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+@section('title','About')
+
+@section('content')
+
+    <div class="container">
+        <div class="content">
+            <div class="title text-center">About Page</div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+@section('title','Contact')
+
+@section('content')
+
+    <div class="container">
+        <div class="content">
+            <div class="title text-center">Contact Page</div>
+        </div>
+    </div>
+
+@endsection
